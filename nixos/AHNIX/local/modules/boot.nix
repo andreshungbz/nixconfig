25a0fd067d3616a4e253f8679ac4e4e692803403
Andres Hung <andres.hung@outlook.com>
2025-05-23 13:37:39 -0600

    Change boot timeout to 5

--- nixos/AHNIX/local/modules/boot.nix
+++ nixos/AHNIX/local/modules/boot.nix
@@ -1,19 +1,19 @@
 # NixOS Boot Configuration
 # GRUB Configuration
 
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   boot = {
     loader = {
       efi.canTouchEfiVariables = true;
-      timeout = 10;
+      timeout = 5;
       grub = {
         enable = true;
         device = "nodev";
         efiSupport = true;
         useOSProber = true;
         fontSize = 24;
-        font =
-          "${pkgs.nerd-fonts.jetbrains-mono}/share/fonts/truetype/NerdFonts/JetBrainsMono/JetBrainsMonoNerdFont-Regular.ttf";
+        font = "${pkgs.nerd-fonts.jetbrains-mono}/share/fonts/truetype/NerdFonts/JetBrainsMono/JetBrainsMonoNerdFont-Regular.ttf";
         extraEntries = ''
           menuentry "Enter BIOS Setup" {
             fwsetup
