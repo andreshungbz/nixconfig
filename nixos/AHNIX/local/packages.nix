c956621b7f9b43a2196dd073194ae18647867021
Andres Hung <andres.hung@outlook.com>
2025-07-08 15:26:18 -0600

    Remove space

--- nixos/AHNIX/local/packages.nix
+++ nixos/AHNIX/local/packages.nix
@@ -92,7 +92,7 @@ in
         local   all             postgres                                peer
         host    all             all             127.0.0.1/32            scram-sha-256
         host    all             all             ::1/128                 scram-sha-256
-        host    all             all             192.168.18.0/24          scram-sha-256
+        host    all             all             192.168.18.0/24         scram-sha-256
 
         # Optional stricter rules:
         # host    mydb            myappuser       192.168.1.0/24          scram-sha-256
