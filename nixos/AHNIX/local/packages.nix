8d419316bb17f641790a3fd9c166733478b4682c
Andres Hung <andres.hung@outlook.com>
2025-07-08 14:35:38 -0600

    Configure PostgreSQL 17

--- nixos/AHNIX/local/packages.nix
+++ nixos/AHNIX/local/packages.nix
@@ -1,6 +1,6 @@
 # Local Nix Packages
 
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 let
   plasmaPackages = import ./modules/plasma-packages.nix { inherit pkgs; };
 in
@@ -70,6 +70,34 @@ in
   # Multipass
   virtualisation.multipass = {
     enable = true;
+  };
+
+  # PostgreSQL 17
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql_17_jit;
+
+    # server configuration
+    settings = {
+      listen_addresses = lib.mkForce "*";
+      port = 5432;
+      jit = true;
+    };
+
+    # pg_hba.conf
+    authentication = ''
+      # TYPE    DATABASE        USER            ADDRESS                 METHOD
+        local   all             all                                     peer
+        local   all             postgres                                peer
+        host    all             all             127.0.0.1/32            scram-sha-256
+        host    all             all             ::1/128                 scram-sha-256
+        host    all             all             192.168.1.0/24          scram-sha-256
+
+        # Optional stricter rules:
+        # host    mydb            myappuser       192.168.1.0/24          scram-sha-256
+        # hostssl all             all             0.0.0.0/0               scram-sha-256
+        # host    all             all             0.0.0.0/0               reject
+    '';
   };
 
   # Wireshark
