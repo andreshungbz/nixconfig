65377c2d7d0da7916d9ee39f9fe14c023304e914
Andres Hung <andres.hung@outlook.com>
2025-07-14 22:13:27 -0600

    Open ports for RustDesk server

--- nixos/AHNIX/local/modules/network.nix
+++ nixos/AHNIX/local/modules/network.nix
@@ -3,5 +3,18 @@
 {
   networking.hostName = "AHNIX";
   networking.networkmanager.enable = true;
-  networking.firewall.allowedTCPPorts = [ 5432 ];
+
+  networking.firewall.allowedTCPPorts = [
+    5432 # psql
+    21114 # web console (Pro)
+    21115 # NAT type test
+    21116 # TCP hole punching and connection service
+    21117 # relay service
+    21118 # websocket (signal)
+    21119 # websocket (relay)
+  ];
+
+  networking.firewall.allowedUDPPorts = [
+    21116 # UDP heartbeat & ID registration
+  ];
 }
