2c264de6c3670bee9b8106f5af6a34fa434b29a9
Andres Hung <andres.hung@outlook.com>
2025-08-09 16:22:47 -0600

    Configure a simple Samba share

--- nixos/shared/modules/avahi.nix
+++ nixos/shared/modules/avahi.nix
@@ -1,6 +1,51 @@
-# NixOS mDNS Configuration
+# NixOS mDNS & Samba Configuration
+# https://wiki.nixos.org/wiki/Samba
 
 {
+  services.samba = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      global = {
+        "workgroup" = "WORKGROUP";
+        "server string" = "smbnix";
+        "netbios name" = "smbnix";
+        "security" = "user";
+        # "use sendfile" = "yes";
+        # "max protocol" = "smb2";
+        # note: localhost is the ipv6 localhost ::1
+        "hosts allow" = "192.168.0.0/16 127.0.0.1 localhost";
+        "hosts deny" = "0.0.0.0/0";
+        "guest account" = "nobody";
+        "map to guest" = "bad user";
+      };
+      "public" = {
+        "path" = "/mnt/Shares/Public";
+        "browseable" = "yes";
+        "read only" = "yes";
+        "guest ok" = "yes";
+        "create mask" = "0444";
+        "directory mask" = "0555";
+      };
+      "private" = {
+        "path" = "/mnt/Shares/Private";
+        "browseable" = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+      };
+    };
+  };
+
+  services.samba-wsdd = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  networking.firewall.enable = true;
+  networking.firewall.allowPing = true;
+
   services.avahi = {
     nssmdns4 = true;
     enable = true;
@@ -9,6 +54,7 @@
     publish = {
       enable = true;
       addresses = true;
+      userServices = true;
       workstation = true;
     };
   };
