980c7030b96bc1b887df6c7076632b6754f52fa5
Andres Hung <andres.hung@outlook.com>
2025-12-13 16:27:43 -0600

    Fix missing programs.

--- pi/AHRP5/home.nix
+++ pi/AHRP5/home.nix
@@ -61,7 +61,7 @@
 
   # FastFetch
 
-  fastfetch = {
+  programs.fastfetch = {
     enable = true;
     settings = {
       "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
