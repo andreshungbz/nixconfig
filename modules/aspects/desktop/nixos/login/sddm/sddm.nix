ea2384dc67fc448e70da7af310379f97473183bd
Andres Hung <andres.hung@outlook.com>
2026-05-21 15:33:21 -0600

    refactor: Move pixie theme into its own thing

--- modules/aspects/desktop/nixos/login/sddm/sddm.nix
+++ modules/aspects/desktop/nixos/login/sddm/sddm.nix
@@ -1,49 +1,15 @@
-{ inputs, lib, ... }:
+{ __findFile, ... }:
 {
-  flake-file.inputs = {
-    # https://github.com/xCaptaiN09/pixie-sddm
-    pixie-sddm.url = "github:xCaptaiN09/pixie-sddm";
-
-    # font fix branch
-    # pixie-sddm.url = "github:johnkferguson/pixie-sddm/fix/honor-config-keys";
-  };
-
   pkt.sddm = {
-    nixos =
-      { pkgs, ... }:
-      {
-        imports = [
-          (inputs.pixie.sddm.nixosModules.default or { })
-        ];
+    includes = [
+      <pkt/sddm-pixie>
+    ];
 
-        services.displayManager.sddm = {
-          enable = true;
-          wayland.enable = true;
-
-          package = lib.mkForce pkgs.kdePackages.sddm;
-
-          extraPackages = with pkgs; [
-            kdePackages.qtsvg
-            kdePackages.qtdeclarative
-            kdePackages.qt5compat
-          ];
-
-          theme = "pixie";
-        };
-
-        environment.systemPackages = [
-          (inputs.pixie-sddm.packages.${pkgs.stdenv.hostPlatform.system}.pixie-sddm.override {
-            background = ./background.jpg;
-            avatar = ./avatar.png;
-            autoColor = true;
-            use24HourClock = false;
-
-            # waiting for font issues to be resolved
-            # https://github.com/xCaptaiN09/pixie-sddm/pull/11
-            # fontFamily = "JetBrains Mono";
-            # fontSpacing = -70;
-          })
-        ];
+    nixos = {
+      services.displayManager.sddm = {
+        enable = true;
+        wayland.enable = true;
       };
+    };
   };
 }
