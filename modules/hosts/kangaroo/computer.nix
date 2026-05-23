0037423608396bb32bb2ded639505f71c7297ff4
Andres Hung <andres.hung@outlook.com>
2026-05-23 17:12:21 -0600

    Add more options

--- modules/hosts/kangaroo/computer.nix
+++ modules/hosts/kangaroo/computer.nix
@@ -34,6 +34,10 @@
 
         hardware = {
           amdgpu.opencl.enable = true;
+          graphics = {
+            enable = true;
+            enable32Bit = true;
+          };
         };
 
         zramSwap.enable = true;
