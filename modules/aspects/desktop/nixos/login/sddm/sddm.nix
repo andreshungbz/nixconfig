b6114b4fc46955264aa643de19c15aa1c07932b0
Andres Hung <andres.hung@outlook.com>
2026-05-24 12:28:05 -0600

    Fix cursor theme not showing

--- modules/aspects/desktop/nixos/login/sddm/sddm.nix
+++ modules/aspects/desktop/nixos/login/sddm/sddm.nix
@@ -8,17 +8,22 @@
     nixos =
       { pkgs, ... }:
       {
-        environment.systemPackages = with pkgs; [
-          kdePackages.breeze
-        ];
+        environment.systemPackages = with pkgs; [ kdePackages.breeze ];
 
         services.displayManager.sddm = {
           enable = true;
           wayland.enable = true;
 
+          extraPackages = with pkgs; [ kdePackages.breeze ];
+
           settings = {
+            General = {
+              GreeterEnvironment = "QT_SCREEN_SCALE_FACTORS=1.25,XCURSOR_THEME=Breeze_Light,XCURSOR_SIZE=24";
+            };
+
             Theme = {
-              CursorTheme = "Breeze_Light";
+              CursorTheme = "breeze_cursors";
+              CursorSize = 24;
             };
           };
         };
