3fe110a9d8ab93a8be76b92a67f570e7b8a45ef6
Andres Hung <andres.hung@outlook.com>
2026-05-24 16:16:20 -0600

    Add opacity, blur, and padding

--- modules/aspects/apps/kitty.nix
+++ modules/aspects/apps/kitty.nix
@@ -15,6 +15,12 @@
           name = "JetBrainsMono Nerd Font";
           size = 11;
         };
+
+        settings = {
+          background_opacity = 0.75;
+          background_blur = 1;
+          window_padding_width = 8;
+        };
       };
     };
   };
