31b5329a90e4bd4e1b4fc78eda8b32ab8dd2aa08
Andres Hung <andres.hung@outlook.com>
2026-05-25 17:10:06 -0600

    Adding to shell inits

--- modules/aspects/apps/homebrew.nix
+++ modules/aspects/apps/homebrew.nix
@@ -2,6 +2,18 @@
   pkt.homebrew = {
     darwin = {
       homebrew.enable = true;
+
+      home-manager = {
+        programs.fish.interactiveShellInit = ''
+          eval "$(/opt/homebrew/bin/brew shellenv)"
+        '';
+        programs.zsh.initContent = ''
+          eval "$(/opt/homebrew/bin/brew shellenv)"
+        '';
+        programs.bash.initExtra = ''
+          eval "$(/opt/homebrew/bin/brew shellenv)"
+        '';
+      };
     };
   };
 }
