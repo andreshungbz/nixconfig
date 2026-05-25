da61698cf42e228ede94a94d25d4b6b031858357
Andres Hung <andres.hung@outlook.com>
2026-05-25 17:13:46 -0600

    Enable integration with shells

--- modules/aspects/apps/homebrew.nix
+++ modules/aspects/apps/homebrew.nix
@@ -1,18 +1,11 @@
 {
   pkt.homebrew = {
     darwin = {
-      homebrew.enable = true;
-
-      home-manager = {
-        programs.fish.interactiveShellInit = ''
-          eval "$(/opt/homebrew/bin/brew shellenv)"
-        '';
-        programs.zsh.initContent = ''
-          eval "$(/opt/homebrew/bin/brew shellenv)"
-        '';
-        programs.bash.initExtra = ''
-          eval "$(/opt/homebrew/bin/brew shellenv)"
-        '';
+      homebrew = {
+        enable = true;
+        enableBashIntegration = true;
+        enableZshIntegration = true;
+        enableFishIntegration = true;
       };
     };
   };
