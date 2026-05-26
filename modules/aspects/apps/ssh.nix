d2c42c5a45d4f2a4f243dcf3ab8deb0406869137
Andres Hung <andres.hung@outlook.com>
2026-05-25 19:02:34 -0600

    Simplify host

--- modules/aspects/apps/ssh.nix
+++ modules/aspects/apps/ssh.nix
@@ -31,14 +31,10 @@
           enable = true;
           enableDefaultConfig = false;
 
-          matchBlocks = {
+          settings = {
             "*.ts.net" = {
               identitiesOnly = true;
-              identityFile = [
-                "${pkgs.writeText "id_ed25519.pub" ''
-                  ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJSHAzMVnHblW0xy4tdMxCZBpEsDRlh+khOMmYzJs5K/
-                ''}"
-              ];
+              identityFile = "~/.ssh/id_ed25519.pub";
             };
           };
         };
