129432da090cd9c7eb007749228127f3aed37d23
Andres Hung <andres.hung@outlook.com>
2026-05-26 10:44:54 -0600

    Move and refactor cursors and icons

--- modules/aspects/desktop/components/nixos/icons/papirus.nix
+++ modules/aspects/desktop/components/nixos/icons/papirus.nix
@@ -3,17 +3,13 @@
     nixos =
       { pkgs, ... }:
       {
-        environment.systemPackages = with pkgs; [
-          papirus-icon-theme
-        ];
+        environment.systemPackages = with pkgs; [ papirus-icon-theme ];
       };
 
     homeManager = {
       gtk = {
         enable = true;
-        iconTheme = {
-          name = "Papirus";
-        };
+        iconTheme.name = "Papirus";
       };
     };
   };
