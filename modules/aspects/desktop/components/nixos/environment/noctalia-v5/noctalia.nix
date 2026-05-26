c970f0d03ee1033445b8d6ad5bcba09315ca3f5b
Andres Hung <andres.hung@outlook.com>
2026-05-26 10:15:22 -0600

    Refactor and add avatars

--- modules/aspects/desktop/components/nixos/environment/noctalia-v5/noctalia.nix
+++ modules/aspects/desktop/components/nixos/environment/noctalia-v5/noctalia.nix
@@ -1,16 +1,20 @@
 { inputs, lib, ... }:
 {
+  # https://docs.noctalia.dev/v5/
   flake-file.inputs = {
-    noctalia = {
-      url = "github:noctalia-dev/noctalia-shell/v5";
-    };
+    noctalia.url = "github:noctalia-dev/noctalia-shell/v5";
   };
 
-  # https://docs.noctalia.dev/v5/
   pkt.noctalia = {
     nixos =
       { pkgs, ... }:
       {
+        # binary cache
+        nix.settings = {
+          substituters = [ "https://noctalia.cachix.org" ];
+          trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
+        };
+
         environment.systemPackages = [
           inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
         ];
@@ -18,22 +22,16 @@
         services = {
           power-profiles-daemon.enable = true; # power profiles
           upower.enable = true; # battery
-        };
-
-        # binary cache
-        nix.settings = {
-          substituters = [ "https://noctalia.cachix.org" ];
-          trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
+          # networkmanager and bluetooth already enabled in nixos-base
         };
       };
 
     homeManager = {
-      imports = [
-        (inputs.noctalia.homeModules.default or { })
-      ];
+      imports = [ inputs.noctalia.homeModules.default ];
 
-      # repository wallpapers
-      home.file."Pictures/Wallpapers/nixconfig-wallpapers".source = ./nixconfig-wallpapers;
+      # repository avatars/wallpapers
+      home.file."Pictures/nixconfig-avatars".source = ./avatars;
+      home.file."Pictures/Wallpapers/nixconfig-wallpapers".source = ./wallpapers;
 
       programs.noctalia = {
         enable = true;
