6d53e22f55237fa9f342ce841a61c4a718aab27b
Andres Hung <andres.hung@outlook.com>
2026-05-25 18:57:37 -0600

    Add matchblock for tailscale

--- modules/aspects/apps/ssh.nix
+++ modules/aspects/apps/ssh.nix
@@ -14,21 +14,34 @@
       };
     };
 
-    homeManager = {
-      home.file = {
-        ".ssh/id_ed25519.pub".text = ''
-          ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJSHAzMVnHblW0xy4tdMxCZBpEsDRlh+khOMmYzJs5K/
-        '';
+    homeManager =
+      { pkgs, ... }:
+      {
+        home.file = {
+          ".ssh/id_ed25519.pub".text = ''
+            ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJSHAzMVnHblW0xy4tdMxCZBpEsDRlh+khOMmYzJs5K/
+          '';
 
-        ".ssh/authorized_keys".text = ''
-          ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJSHAzMVnHblW0xy4tdMxCZBpEsDRlh+khOMmYzJs5K/
-        '';
-      };
+          ".ssh/authorized_keys".text = ''
+            ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJSHAzMVnHblW0xy4tdMxCZBpEsDRlh+khOMmYzJs5K/
+          '';
+        };
 
-      programs.ssh = {
-        enable = true;
-        enableDefaultConfig = false;
+        programs.ssh = {
+          enable = true;
+          enableDefaultConfig = false;
+
+          matchBlocks = {
+            "*.ts.net" = {
+              identitiesOnly = true;
+              identityFile = [
+                "${pkgs.writeText "id_ed25519.pub" ''
+                  ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJSHAzMVnHblW0xy4tdMxCZBpEsDRlh+khOMmYzJs5K/
+                ''}"
+              ];
+            };
+          };
+        };
       };
-    };
   };
 }
