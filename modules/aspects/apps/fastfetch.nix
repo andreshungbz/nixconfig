f58748fd43e1b55edcf24d298602a802e938c787
Andres Hung <andres.hung@outlook.com>
2026-05-31 11:30:46 -0600

    Add a space to DE

--- modules/aspects/apps/fastfetch.nix
+++ modules/aspects/apps/fastfetch.nix
@@ -58,7 +58,7 @@
             }
             {
               type = "de";
-              key = "Desktop Environment";
+              key = " Desktop Environment";
             }
             {
               type = "wm";
