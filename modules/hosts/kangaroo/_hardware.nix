94d2f7beb1a21cbd1a7b629b9fd89e6d498b7ee3
Andres Hung <andres.hung@outlook.com>
2026-06-01 15:00:42 -0600

    Remove filesystem specifications in preparation to use disko

--- modules/hosts/kangaroo/_hardware.nix
+++ modules/hosts/kangaroo/_hardware.nix
@@ -22,25 +22,10 @@
     "usb_storage"
     "sd_mod"
   ];
+
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/582ee604-e4b5-4fc7-8a0b-5b02a3ce9d0f";
-    fsType = "ext4";
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/7165-35C6";
-    fsType = "vfat";
-    options = [
-      "fmask=0077"
-      "dmask=0077"
-    ];
-  };
-
-  swapDevices = [ ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
