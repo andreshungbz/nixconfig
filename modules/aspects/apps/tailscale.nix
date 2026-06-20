33506418d3006eedc7a59d82b916640ec29f7cff
Andres Hung <andres.hung@outlook.com>
2026-06-19 21:32:01 -0600

    Change tailscale back to cask

--- modules/aspects/apps/tailscale.nix
+++ modules/aspects/apps/tailscale.nix
@@ -13,7 +13,7 @@
       # need brew for Tailscale SSH Console, but MagicDNS currently not working
       # https://github.com/tailscale/tailscale/issues/13461
       # workaround: echo "nameserver 100.100.100.100" | sudo tee /etc/resolver/ts.net
-      homebrew.brews = [ "tailscale" ];
+      homebrew.casks = [ "tailscale-app" ];
     };
   };
 }
