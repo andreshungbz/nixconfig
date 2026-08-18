14ddf469de1d7a78085a6ffdbe0a470155d9c5d6
Andres Hung <2018118240@ub.edu.bz>
2026-08-18 09:29:19 -0600

    Configure startup programs with spawn-at-startup

--- modules/aspects/desktop/niri/settings/startup.nix
+++ modules/aspects/desktop/niri/settings/startup.nix
@@ -8,9 +8,12 @@
         hotkey-overlay.skip-at-startup = true;
 
         spawn-at-startup = [
-          {
-            _args = [ "noctalia" ];
-          }
+          { _args = [ "noctalia" ]; }
+          { _args = [ "Telegram" ]; }
+          { _args = [ "discord" ]; }
+          { _args = [ "discordptb" ]; }
+          { _args = [ "discordcanary" ]; }
+          { _args = [ "steam" ]; }
         ];
       };
     };
